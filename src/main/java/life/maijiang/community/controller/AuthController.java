a0625588da154fb8781f540e2c3e29a0f998a30f
yangzhang123456 <332446290@qq.com>
2019-08-20 01:00:47 +0800

    before session

--- src/main/java/life/maijiang/community/controller/AuthController.java
+++ src/main/java/life/maijiang/community/controller/AuthController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthController {
     @Autowired
@@ -22,7 +25,8 @@ public class AuthController {
     private String Redirect_uri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setCode(code);
@@ -33,7 +37,13 @@ public class AuthController {
         System.out.println(accessToken);
         GithubUser user = gitHubProvider.getUser(accessToken);
         System.out.println(user.getName());
-        return "index";
+        if(user!=null){//登陆成功写入session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";//重定向
+        }else {
+            return "redirect:/";//重定向
+        }
+
     }
 
 }
